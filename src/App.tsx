803bba4a682f2276840547959ab4e4310b042b3f
Alexander Thomas <alexmalt001@live.co.uk>
2023-10-08 20:51:35 +0100

    Removed green testing text formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 export function App() {
-    return <h1 className="grid place-content-center text-center min-h-screen text-red-500 text-5xl sm:text-green-500 sm:text-8xl lg:text-9xl font-bold">
+    return <h1 className="grid place-content-center text-center min-h-screen text-red-500 text-5xl sm:text-8xl lg:text-9xl font-bold">
        🚧 ⚠️  🚧<br/>
        Site under construction
 
