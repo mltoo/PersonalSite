c95a7f25d9ce1dacd1e6e0e4eb3e9f3beea8036b
Alexander Thomas <alexmalt001@live.co.uk>
2023-10-12 21:50:13 +0100

    Adaptive content column size across all media types

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
 import * as React from 'react';
 import Sidebar from './Sidebar';
-import { useTWBreakpoints, useMediaQueries} from './twUtils';
+import { useMediaQueries} from './twUtils';
 
 export function App() {
-    const activeBreakpoints = useTWBreakpoints();
     const mq = useMediaQueries();
-    return <div className="lg:grid lg:grid-cols-2">
+    return <div className="lg:grid" style={mq<object>([{},
+    {
+        lg: {
+            gridTemplateColumns: "1fr auto 0fr"
+            },
+        xl: {
+            gridTemplateColumns: "1fr auto 0.5fr"
+            },
+        "2xl":{
+            grimTemplateColumns: "1fr auto 1fr"
+            }
+    }])}>
         <Sidebar className='lg:col-start-1 lg:row-start-1 lg:row-end-1'/>
-        <div className='lg:col-start-2 lg:row-start-1 lg:row-end-1'>
-            test
-            <div className="h-96 bg-red-500"/> 
-            <div className="h-96 bg-green-500"/>
-            <div className="h-96 bg-blue-500"/>
-            <div className="h-96 bg-orange-500"/>
-        </div>
-        <div className="fixed bottom-5 right-5 bg-red-300 rounded-md px-2 bg-opacity-50">
-            {activeBreakpoints.toString()}<br/>
-            {mq(['DEFAULT', {'lg':'LARGE'}])}<br/>
+        <div className='min-h-[150vh] lg:min-h-auto px-4 pt-4 lg:w-[45rem] xl:[55rem] lg:col-start-2 lg:row-start-1 lg:row-end-1'>
         </div>
     </div>;
 
